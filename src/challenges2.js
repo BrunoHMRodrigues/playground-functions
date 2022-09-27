59c78a1ff98ae1b5ed7619e8aa238eec560ccc5a
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-27 19:19:14 -0300

    Realizado Requisito Bônus 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,67 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumeros) {
+  let teleNumber = "";
+  let testValidation = true;
+  const contadorRepetidor = contarRepeticao(arrayNumeros);
+
+  if (arrayNumeros.length !== 11) {
+    return "Array com tamanho incorreto.";
+    testValidation = false;
+  } else {
+    for (let index = 0; index < arrayNumeros.length; index+=1) {
+      
+      if (arrayNumeros[index] < 0 || arrayNumeros[index] > 9 || contadorRepetidor ===true) {
+        return "não é possível gerar um número de telefone com esses valores";
+        testValidation = false;
+      }
+      
+    }
+
+
+  }
+  if (testValidation === true) {
+    for (let index = 0; index < arrayNumeros.length; index+=1) {
+      if (index === 0) {
+        teleNumber += "(" + arrayNumeros[index];
+      } else if (index === 1) {
+        teleNumber += arrayNumeros[index] + ") ";
+      } else if (index < 7) {
+        teleNumber += arrayNumeros[index];
+      } else if (index === 7) {
+        teleNumber += "-" + arrayNumeros[index];
+      } else {
+        teleNumber += arrayNumeros[index];
+      }
+          
+    }
+  }  
+
+  return teleNumber;
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 3, 7, 8, 9, 0, 1]));
+
+function contarRepeticao(arrayNumeros) {
+  let contadorRepeticao = 0;
+  let testeMaiorIgual3 = false;
+
+  for (let index = 0; index < arrayNumeros.length; index+=1) {
+    for (let index2 = 0; index2 < arrayNumeros.length; index2+=1) {
+      if (arrayNumeros[index] === arrayNumeros[index2]) {
+        contadorRepeticao +=1
+      }      
+    }
+
+    if (contadorRepeticao >= 3) {
+      testeMaiorIgual3 = true;
+    }
+    contadorRepeticao = 0;
+    
+  }
+
+  return testeMaiorIgual3;
+}
+
+
 
 // Desafio 12
 function triangleCheck() {
